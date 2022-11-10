8e17d5f963417d9575facd46599bfed8f81f8236
xrias <mr.artem.reshetnikov.2017@mail.ru>
2022-11-11 01:12:01 +0300

    Порефачил

---
+++ src/Game.js
@@ -0,0 +1,25 @@
+import { questions } from './constants';
+
+export function Game({ step, question, onClickVariant }) {
+    const percentage = Math.round((step / questions.length) * 100);
+  
+    return (
+      <>
+        <div className="progress">
+            <div style={{ width: `${percentage}%` }} className="progress__inner"/>
+        </div>
+        <h1>
+            {question.title}
+        </h1>
+        <ul>
+          {
+            question.variants.map((text, index) => (
+                <li onClick={() => onClickVariant(index)} key={text}>
+                    {text}
+                </li>
+            ))
+          }
+        </ul>
+      </>
+    );
+  }
